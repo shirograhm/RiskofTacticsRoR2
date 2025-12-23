756849f966c7813a972e5ef33c5d47b94be84f1b
shirograhm <xavierwg13@gmail.com>
2025-12-22 17:28:20 -0800

    Updates

---
+++ RiskOfTactics/Items/Artifacts/GamblersBlade.cs
@@ -0,0 +1,182 @@
+﻿using R2API;
+using RoR2;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace RiskOfTactics.Items.Artifacts
+{
+    class GamblersBlade
+    {
+        public static ItemDef itemDef;
+
+        // Gain 0%-30% additional attack speed based on your current gold ($0-$150). On-hit, 5% chance to drop treasure.
+        public static ConfigurableValue<bool> isEnabled = new(
+            "Item: Gamblers Blade",
+            "Enabled",
+            true,
+            "Whether or not the item is enabled.",
+            new List<string>()
+            {
+                "ITEM_GAMBLERSBLADE_DESC"
+            }
+        );
+        public static ConfigurableValue<float> attackSpeedEffectCap = new(
+            "Item: Gamblers Blade",
+            "Attack Speed Effect Cap",
+            30f,
+            "Amount of attack speed granted at money cap.",
+            new List<string>()
+            {
+                "ITEM_GAMBLERSBLADE_DESC"
+            }
+        );
+        public static ConfigurableValue<float> attackSpeedEffectCapExtraStacks = new(
+            "Item: Gamblers Blade",
+            "Attack Speed Effect Cap Extra Stacks",
+            30f,
+            "Amount of attack speed granted at money cap for extra stacks.",
+            new List<string>()
+            {
+                "ITEM_GAMBLERSBLADE_DESC"
+            }
+        );
+        public static ConfigurableValue<int> moneyEffectCap = new(
+            "Item: Gamblers Blade",
+            "Money Cap",
+            150,
+            "Amount of money required to gain the attack speed cap. Scales with difficulty.",
+            new List<string>()
+            {
+                "ITEM_GAMBLERSBLADE_DESC"
+            }
+        );
+        public static ConfigurableValue<float> moneyDropChance = new(
+            "Item: Gamblers Blade",
+            "Drop Chance",
+            5f,
+            "Percent chance on-hit to gain money.",
+            new List<string>()
+            {
+                "ITEM_GAMBLERSBLADE_DESC"
+            }
+        );
+        public static ConfigurableValue<int> moneyGainOnDrop = new(
+            "Item: Gamblers Blade",
+            "Money Gain",
+            15,
+            "Money gained on drop.",
+            new List<string>()
+            {
+                "ITEM_GAMBLERSBLADE_DESC"
+            }
+        );
+        public static readonly float percentMoneyDropChance = moneyDropChance.Value / 100f;
+
+        internal static void Init()
+        {
+            GenerateItem();
+
+            ItemDisplayRuleDict displayRules = new ItemDisplayRuleDict(null);
+            ItemAPI.Add(new CustomItem(itemDef, displayRules));
+
+            Hooks();
+        }
+
+        private static void GenerateItem()
+        {
+            itemDef = ScriptableObject.CreateInstance<ItemDef>();
+
+            itemDef.name = "GAMBLERSBLADE";
+            itemDef.AutoPopulateTokens();
+
+            Utils.SetItemTier(itemDef, ItemTier.Tier3);
+
+            GameObject prefab = AssetHandler.bundle.LoadAsset<GameObject>("GamblersBlade.prefab");
+            ModelPanelParameters modelPanelParameters = prefab.AddComponent<ModelPanelParameters>();
+            modelPanelParameters.focusPointTransform = prefab.transform;
+            modelPanelParameters.cameraPositionTransform = prefab.transform;
+            modelPanelParameters.maxDistance = 10f;
+            modelPanelParameters.minDistance = 5f;
+
+            itemDef.pickupIconSprite = AssetHandler.bundle.LoadAsset<Sprite>("GamblersBlade.png");
+            itemDef.pickupModelPrefab = prefab;
+            itemDef.canRemove = true;
+            itemDef.hidden = false;
+
+            itemDef.tags = new ItemTag[]
+            {
+                ItemTag.Damage,
+                ItemTag.Utility
+            };
+        }
+
+        public static void Hooks()
+        {
+            RecalculateStatsAPI.GetStatCoefficients += (sender, args) =>
+            {
+                if (sender && sender.inventory)
+                {
+                    int count = sender.inventory.GetItemCountEffective(itemDef);
+                    if (count > 0)
+                    {
+                        if (sender.master && sender.master.money > 0)
+                        {
+                            float moneyRequired = moneyEffectCap.Value * Utils.GetDifficultyAsMultiplier();
+                            // Cap money ratio at 100%
+                            float currentMoneyRatio = Mathf.Min(1f, sender.master.money / moneyRequired);
+                            float attackSpeedBonus = currentMoneyRatio * Utils.GetLinearStacking(attackSpeedEffectCap.Value, attackSpeedEffectCapExtraStacks.Value, count) / 100f;
+
+                            args.attackSpeedMultAdd += attackSpeedBonus;
+                        }
+                    }
+                }
+            };
+
+            GenericGameEvents.OnHitEnemy += (damageInfo, attackerInfo, victimInfo) =>
+            {
+                CharacterBody atkBody = attackerInfo.body;
+                CharacterBody vicBody = victimInfo.body;
+                if (atkBody && atkBody.master && atkBody.inventory && atkBody.inventory.GetItemCountEffective(itemDef) > 0)
+                {
+                    if (Util.CheckRoll0To1(percentMoneyDropChance, atkBody.master.luck))
+                    {
+                        SpawnGoldPack(atkBody, vicBody);
+                    }
+                }
+            };
+        }
+
+        private static void SpawnGoldPack(CharacterBody attacker, CharacterBody victim)
+        {
+            GameObject goldPackObject = Object.Instantiate(LegacyResourcesAPI.Load<GameObject>("Prefabs/NetworkedObjects/BonusMoneyPack"), victim.transform.position, Random.rotation);
+            if (goldPackObject)
+            {
+                Collider component = goldPackObject.GetComponent<Collider>();
+                if (component)
+                {
+                    TeamFilter teamComponent = goldPackObject.GetComponent<TeamFilter>();
+                    if (teamComponent && attacker.teamComponent)
+                    {
+                        teamComponent.teamIndex = attacker.teamComponent.teamIndex;
+                    }
+                    MoneyPickup componentInChildren = goldPackObject.GetComponentInChildren<MoneyPickup>();
+                    if ((bool)componentInChildren)
+                    {
+                        componentInChildren.baseGoldReward = moneyGainOnDrop;
+                        Physics.IgnoreCollision(component, componentInChildren.GetComponent<Collider>());
+                    }
+                    GravitatePickup componentInChildren2 = goldPackObject.GetComponentInChildren<GravitatePickup>();
+                    if ((bool)componentInChildren2)
+                    {
+                        Physics.IgnoreCollision(component, componentInChildren2.GetComponent<Collider>());
+                    }
+                    goldPackObject.transform.localScale = new Vector3(1f, 1.5f, 0.85f);
+
+                    NetworkServer.Spawn(goldPackObject);
+                }
+            }
+        }
+
+    }
+}
---
+++ RiskOfTactics/Items/Artifacts/StatikkShiv.cs
@@ -0,0 +1,251 @@
+using R2API;
+using RoR2;
+using RoR2.Orbs;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RiskOfTactics.Items.Artifacts
+{
+    class StatikkShiv
+    {
+        public static ItemDef itemDef;
+        public static BuffDef shockBuff;
+        public static BuffDef shockCooldown;
+
+        // Your next attack periodically chains to nearby enemies, dealing bonus damage and applying Sunder.
+        public static ConfigurableValue<bool> isEnabled = new(
+            "Item: Statikk Shiv",
+            "Enabled",
+            true,
+            "Whether or not the item is enabled.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static ConfigurableValue<float> effectCooldown = new(
+            "Item: Statikk Shiv",
+            "Effect Cooldown",
+            6f,
+            "Cooldown of this item's effect.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static ConfigurableValue<float> effectOnHitDamage = new(
+            "Item: Statikk Shiv",
+            "Bonus On-Hit",
+            90f,
+            "Bonus on-hit damage (as a percentage of TOTAL damage) dealt by this item's effect.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static ConfigurableValue<float> effectOnHitDamageExtraStacks = new(
+            "Item: Statikk Shiv",
+            "Bonus On-Hit Per Stack",
+            90f,
+            "Bonus on-hit damage for extra stacks of this item.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static ConfigurableValue<int> shivRange = new(
+            "Item: Statikk Shiv",
+            "Zap Range",
+            30,
+            "Zap range of the item's chain effect in meters.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static ConfigurableValue<int> numberEnemiesChained = new(
+            "Item: Statikk Shiv",
+            "Number of Enemies Chained",
+            3,
+            "Number of additional enemies chained off the initial enemy with this effect.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static ConfigurableValue<float> shivProcCoeff = new(
+            "Item: Statikk Shiv",
+            "Proc Coefficient",
+            0.5f,
+            "Proc coefficient for the chain effect of this item.",
+            new List<string>()
+            {
+                "ITEM_STATIKKSHIV_DESC"
+            }
+        );
+        public static float percentEffectOnHitDamage = effectOnHitDamage.Value / 100f;
+        public static float percentEffectOnHitDamageExtraStacks = effectOnHitDamageExtraStacks.Value / 100f;
+
+        internal static void Init()
+        {
+            GenerateItem();
+
+            ItemDisplayRuleDict displayRules = new ItemDisplayRuleDict(null);
+            ItemAPI.Add(new CustomItem(itemDef, displayRules));
+
+            shockBuff = Utils.GenerateBuffDef("Shock", AssetHandler.bundle.LoadAsset<Sprite>("Shock.png"), false, false, false, false);
+            ContentAddition.AddBuffDef(shockBuff);
+            shockCooldown = Utils.GenerateBuffDef("Shock Cooldown", AssetHandler.bundle.LoadAsset<Sprite>("ShockCD.png"), false, false, false, true);
+            ContentAddition.AddBuffDef(shockCooldown);
+
+            Hooks();
+        }
+
+        private static void GenerateItem()
+        {
+            itemDef = ScriptableObject.CreateInstance<ItemDef>();
+
+            itemDef.name = "STATIKKSHIV";
+            itemDef.AutoPopulateTokens();
+
+            Utils.SetItemTier(itemDef, ItemTier.Tier3);
+
+            GameObject prefab = AssetHandler.bundle.LoadAsset<GameObject>("StatikkShiv.prefab");
+            ModelPanelParameters modelPanelParameters = prefab.AddComponent<ModelPanelParameters>();
+            modelPanelParameters.focusPointTransform = prefab.transform;
+            modelPanelParameters.cameraPositionTransform = prefab.transform;
+            modelPanelParameters.maxDistance = 10f;
+            modelPanelParameters.minDistance = 5f;
+
+            itemDef.pickupIconSprite = AssetHandler.bundle.LoadAsset<Sprite>("StatikkShiv.png");
+            itemDef.pickupModelPrefab = prefab;
+            itemDef.canRemove = true;
+            itemDef.hidden = false;
+
+            itemDef.tags = new ItemTag[]
+            {
+                ItemTag.Damage,
+                ItemTag.Utility,
+
+                ItemTag.CanBeTemporary
+            };
+        }
+
+        public static void Hooks()
+        {
+            Stage.onStageStartGlobal += (stage) =>
+            {
+                foreach (PlayerCharacterMasterController controller in PlayerCharacterMasterController.instances)
+                {
+                    if (controller)
+                    {
+                        CharacterMaster master = controller.master;
+                        if (master && master.GetBody() && master.GetBody().inventory && master.GetBody().inventory.GetItemCountEffective(itemDef) > 0)
+                        {
+                            master.GetBody().AddBuff(shockBuff);
+                        }
+                    }
+                }
+            };
+
+            On.RoR2.Inventory.GiveItemPermanent_ItemIndex_int += (orig, self, index, count) =>
+            {
+                orig(self, index, count);
+
+                if (index == itemDef.itemIndex)
+                {
+                    CharacterMaster master = self.GetComponent<CharacterMaster>();
+                    if (master && master.GetBody() && self.GetItemCountEffective(itemDef) > 0)
+                    {
+                        master.GetBody().AddBuff(shockBuff);
+                    }
+                }
+            };
+
+            On.RoR2.Inventory.GiveItemTemp += (orig, self, index, count) =>
+            {
+                orig(self, index, count);
+
+                if (index == itemDef.itemIndex)
+                {
+                    CharacterMaster master = self.GetComponent<CharacterMaster>();
+                    if (master && master.GetBody() && self.GetItemCountEffective(itemDef) > 0)
+                    {
+                        master.GetBody().AddBuff(shockBuff);
+                    }
+                }
+            };
+
+            On.RoR2.CharacterBody.OnBuffFinalStackLost += (orig, self, buffDef) =>
+            {
+                orig(self, buffDef);
+
+                if (buffDef == shockCooldown)
+                {
+                    if (self && self.inventory && self.inventory.GetItemCountEffective(itemDef) > 0 && self.GetBuffCount(shockBuff) == 0)
+                    {
+                        self.AddBuff(shockBuff);
+                    }
+                }
+            };
+
+            GenericGameEvents.BeforeTakeDamage += (damageInfo, attackerInfo, victimInfo) =>
+            {
+                CharacterBody vicBody = victimInfo.body;
+                CharacterBody atkBody = attackerInfo.body;
+
+                if (vicBody && atkBody && atkBody.inventory)
+                {
+                    int count = atkBody.inventory.GetItemCountEffective(itemDef);
+                    bool hasShockBuff = atkBody.GetBuffCount(shockBuff) > 0;
+                    if (hasShockBuff && !Utils.OnSameTeam(vicBody, atkBody))
+                    {
+                        float damageMultiplier = 1 + Utils.GetLinearStacking(percentEffectOnHitDamage, percentEffectOnHitDamageExtraStacks, count);
+                        damageInfo.damage *= damageMultiplier;
+                        damageInfo.damageColorIndex = DamageColorIndex.WeakPoint;
+                        damageInfo.AddModdedDamageType(Sunder.ApplySunder);
+
+                        AddStatikkChainDamage(atkBody, vicBody, damageInfo, damageMultiplier);
+
+                        vicBody.AddBuff(Sunder.buffDef);
+
+                        // Remove the shock buff and add cooldown buff
+                        atkBody.RemoveBuff(shockBuff);
+                        atkBody.AddTimedBuff(shockCooldown, effectCooldown.Value);
+                    }
+                }
+            };
+        }
+
+        private static void AddStatikkChainDamage(CharacterBody attacker, CharacterBody victim, DamageInfo damageInfo, float mult)
+        {
+            if (attacker.teamComponent != null)
+            {
+                LightningOrb orb = new()
+                {
+                    origin = damageInfo.position,
+                    damageValue = damageInfo.damage * mult,
+                    isCrit = damageInfo.crit,
+                    bouncesRemaining = numberEnemiesChained.Value,
+                    teamIndex = attacker.teamComponent.teamIndex,
+                    attacker = attacker.gameObject,
+                    bouncedObjects = [victim.GetComponent<HealthComponent>()],
+                    procChainMask = damageInfo.procChainMask,
+                    procCoefficient = shivProcCoeff,
+                    lightningType = LightningOrb.LightningType.MageLightning,
+                    damageColorIndex = DamageColorIndex.WeakPoint,
+                    range = shivRange,
+                    canBounceOnSameTarget = false
+                };
+                orb.AddModdedDamageType(Sunder.ApplySunder);
+
+                HurtBox hb = orb.PickNextTarget(damageInfo.position);
+                if (hb)
+                {
+                    orb.target = hb;
+                    OrbManager.instance.AddOrb(orb);
+                }
+            }
+        }
+    }
+}
