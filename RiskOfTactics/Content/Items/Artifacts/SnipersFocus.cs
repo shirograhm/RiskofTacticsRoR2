22e4bbfd9d79ac2cbeef22cea6faa64f49a22993
shirograhm <xavierwg13@gmail.com>
2026-04-02 21:59:22 -0700

    Create SnipersFocus.cs

---
+++ RiskOfTactics/Content/Items/Artifacts/SnipersFocus.cs
@@ -0,0 +1,72 @@
+﻿using RiskOfTactics.Managers;
+using RoR2;
+using UnityEngine;
+
+namespace RiskOfTactics.Content.Items.Artifacts
+{
+    public class SnipersFocus
+    {
+        public static ItemDef itemDef;
+
+        // Deal more damage to targets that are far away.
+        public static ConfigurableValue<bool> isEnabled = new(
+            "Item: Snipers Focus",
+            "Enabled",
+            true,
+            "Whether or not the item is enabled.",
+            ["ITEM_ROT_SNIPERSFOCUS_DESC"]
+        );
+        public static ConfigurableValue<float> damageIncreasePerMeter = new(
+            "Item: Snipers Focus",
+            "Damage Increase",
+            1f,
+            "Percent of additional damage dealt per meter of distance.",
+            ["ITEM_ROT_SNIPERSFOCUS_DESC"]
+        );
+        public static ConfigurableValue<float> damageIncreasePerMeterExtraStacks = new(
+            "Item: Snipers Focus",
+            "Damage Increase Extra Stacks",
+            1f,
+            "Percent of additional damage dealt per meter of distance for additional item stacks.",
+            ["ITEM_ROT_SNIPERSFOCUS_DESC"]
+        );
+        public static ConfigurableValue<float> maxDistance = new(
+            "Item: Snipers Focus",
+            "Max Distance",
+            100f,
+            "Maximum distance at which the damage increase is applied.",
+            ["ITEM_ROT_SNIPERSFOCUS_DESC"]
+        );
+        public static readonly float percentDamageIncreasePerMeter = damageIncreasePerMeter.Value / 100f;
+        public static readonly float percentDamageIncreasePerMeterExtraStacks = damageIncreasePerMeterExtraStacks.Value / 100f;
+
+        internal static void Init()
+        {
+            itemDef = ItemManager.GenerateItem("SnipersFocus", [ItemTag.Damage, ItemTag.CanBeTemporary], ItemManager.TacticTier.Artifact);
+
+            Hooks();
+        }
+
+        public static void Hooks()
+        {
+            GameEventManager.BeforeTakeDamage += (damageInfo, attackerInfo, victimInfo) =>
+            {
+                CharacterBody atkBody = attackerInfo.body;
+                GameObject victimObject = victimInfo.gameObject;
+
+                if (atkBody && victimObject && atkBody.inventory)
+                {
+                    int count = atkBody.inventory.GetItemCountEffective(itemDef);
+                    if (count > 0)
+                    {
+                        float distance = Vector3.Distance(attackerInfo.aimOrigin, victimObject.transform.position);
+                        float totalPercentage = Utilities.GetLinearStacking(percentDamageIncreasePerMeter, percentDamageIncreasePerMeterExtraStacks, count);
+                        float damageIncrease = Mathf.Min(distance * totalPercentage, maxDistance.Value * totalPercentage);
+
+                        damageInfo.damage *= 1f + damageIncrease;
+                    }
+                }
+            };
+        }
+    }
+}
