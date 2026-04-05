69f9c24f839ee5e138b0152429c6fa7dd1865000
shirograhm <xavierwg13@gmail.com>
2026-04-05 02:38:38 -0700

    Create ZhonyasParadox.cs

---
+++ RiskOfTactics/Content/Items/Artifacts/ZhonyasParadox.cs
@@ -0,0 +1,87 @@
+﻿using RiskOfTactics.Managers;
+using RoR2;
+using UnityEngine;
+
+namespace RiskOfTactics.Content.Items.Artifacts
+{
+    internal class ZhonyasParadox
+    {
+        public static ItemDef itemDef;
+
+        // Taking damage below a threshold causes you to become Gilded for a duration.
+        public static ConfigurableValue<bool> isEnabled = new(
+            "Item: Zhonyas Paradox",
+            "Enabled",
+            true,
+            "Whether or not the item is enabled.",
+            ["ITEM_ROT_ZHONYASPARADOX_DESC"]
+        );
+        public static ConfigurableValue<float> healthThreshold = new(
+            "Item: Zhonyas Paradox",
+            "Health Threshold",
+            40f,
+            "Percent health threshold at which the effect activates.",
+            ["ITEM_ROT_ZHONYASPARADOX_DESC"]
+        );
+        public static ConfigurableValue<float> effectDuration = new(
+            "Item: Zhonyas Paradox",
+            "Effect Duration",
+            3f,
+            "Duration of the Gilded effect.",
+            ["ITEM_ROT_ZHONYASPARADOX_DESC"]
+        );
+        public static ConfigurableValue<float> effectDurationExtraStacks = new(
+            "Item: Zhonyas Paradox",
+            "Effect Duration Extra Stacks",
+            3f,
+            "Additional duration of the Gilded effect with extra stacks.",
+            ["ITEM_ROT_ZHONYASPARADOX_DESC"]
+        );
+        public static readonly float percentHealthThreshold = healthThreshold.Value / 100f;
+
+        internal static void Init()
+        {
+            itemDef = ItemManager.GenerateItem("ZhonyasParadox", [ItemTag.Utility, ItemTag.CanBeTemporary], ItemManager.TacticTier.Artifact);
+
+            Hooks();
+        }
+
+        public static void Hooks()
+        {
+            GameEventManager.OnTakeDamage += (damageReport) =>
+            {
+                CharacterBody vicBody = damageReport.victimBody;
+                if (vicBody && vicBody.inventory)
+                {
+                    int count = vicBody.inventory.GetItemCountEffective(itemDef);
+                    if (count > 0 && vicBody.healthComponent)
+                    {
+                        if (vicBody.healthComponent.healthFraction <= percentHealthThreshold)
+                        {
+                            float duration = Utilities.GetLinearStacking(effectDuration.Value, effectDurationExtraStacks.Value, count);
+                            vicBody.AddTimedBuff(DLC2Content.Buffs.EliteAurelionite, duration);
+                        }
+                    }
+                }
+            };
+
+            GameEventManager.BeforeTakeDamage += (damageInfo, attackerInfo, victimInfo) =>
+            {
+                CharacterBody vicBody = victimInfo.body;
+                if (vicBody && vicBody.inventory)
+                {
+                    int count = vicBody.inventory.GetItemCountEffective(itemDef);
+                    if (count > 0 && vicBody.healthComponent)
+                    {
+                        float resultingHealth = Mathf.Max(vicBody.healthComponent.combinedHealth - damageInfo.damage, 0f);
+                        if (resultingHealth < 0 && vicBody.healthComponent.healthFraction > percentHealthThreshold)
+                        {
+                            // Damage the player down to the threshold instead of killing them
+                            damageInfo.damage = resultingHealth - vicBody.healthComponent.fullCombinedHealth * percentHealthThreshold;
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
