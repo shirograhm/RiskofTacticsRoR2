6939ffab02144c05c26616e9216b834e16781733
shirograhm <xavierwg13@gmail.com>
2026-04-12 07:50:08 -0700

    Remove item

--- RiskOfTactics/RiskOfTactics.cs
+++ RiskOfTactics/RiskOfTactics.cs
@@ -91,8 +91,6 @@ namespace RiskOfTactics
             // Artifacts
             if (GamblersBlade.isEnabled.Value)
                 GamblersBlade.Init();
-            if (GoldCollector.isEnabled.Value)
-                GoldCollector.Init();
             if (HellfireHatchet.isEnabled.Value)
                 HellfireHatchet.Init();
             if (HorizonFocus.isEnabled.Value)
