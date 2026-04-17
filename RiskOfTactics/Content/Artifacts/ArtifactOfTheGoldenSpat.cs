1dfa70fcfc2b92d9c74a554676eb29b6812bbe66
shirograhm <xavierwg13@gmail.com>
2026-04-17 02:53:37 -0700

    Refactor artifact

--- RiskOfTactics/Content/Artifacts/ArtifactOfTheGoldenSpat.cs
+++ RiskOfTactics/Content/Artifacts/ArtifactOfTheGoldenSpat.cs
@@ -12,7 +12,7 @@ namespace RiskOfTactics.Content.Artifacts
 
         public static GameObject PickupModelPrefab { get; } = null;
 
-        // Spawns a random assortment of TFT items on use.
+        // Start with a random normal-tier TFT item. Every time you level up, replace your current equipment with a Lucky Item Chest.
         public static ConfigurableValue<bool> isEnabled = new(
             "Artifact of the Golden Spatula",
             "Enabled",
@@ -40,17 +40,30 @@ namespace RiskOfTactics.Content.Artifacts
         {
             On.RoR2.Run.BeginStage += (orig, self) =>
             {
-                if (RunArtifactManager.instance.IsArtifactEnabled(artifactDef))
+                if (RunArtifactManager.instance.IsArtifactEnabled(artifactDef) && Run.instance.stageClearCount == 0)
                 {
                     foreach (var player in PlayerCharacterMasterController.instances)
                     {
                         if (player && player.master && player.master.inventory)
                         {
                             var inventory = player.master.inventory;
-                            inventory.SetEquipmentIndexForSlot(LuckyItemChest.equipmentDef.equipmentIndex, 0, 0);
+                            inventory.GiveItemPermanent(ItemManager.GetRandomTacticItemOfTier(ItemManager.TacticTier.Normal));
                         }
                     }
                 }
+                orig(self);
+            };
+
+            On.RoR2.CharacterBody.OnLevelUp += (orig, self) =>
+            {
+                if (RunArtifactManager.instance.IsArtifactEnabled(artifactDef))
+                {
+                    if (self && self.inventory)
+                    {
+                        self.inventory.SetEquipmentIndexForSlot(LuckyItemChest.equipmentDef.equipmentIndex, 0, 0);
+                    }
+                }
+
                 orig(self);
             };
         }
